d55df3c8e94393d7ebe4d9679f390bcc41470153
Александр Гренишин <nd0ut.me@gmail.com>
2018-01-31 14:03:36 +0300

    implement camera smooth following

--- js/CameraController.js
+++ js/CameraController.js
@@ -1,67 +1,36 @@
-import everpolate from 'everpolate';
-import { Vec2 } from './math';
+import { lerp, Vec2 } from './math';
 
 export class CameraController {
     constructor(camera) {
-        this.camera = camera;
-
-        this.lastPos = camera.pos.clone();
-        this.lastVel = undefined;
-
-        this.velocityChangeTime = 0;
-
+        this.cam = camera;
         this.cameraOffset = new Vec2(0, 0);
-    }
-
-    get pos() {
-        return this.camera.pos;
+        this.damping = 0.5;
     }
 
     setFollowEntity(entity) {
         this.entity = entity;
-        this.lastVel = entity.vel.clone();        
+        this.lastVel = entity.vel.clone();
     }
 
-    update(deltaTime, time, {alcohol = false, earthquake = false} = {}) {
-        this.setBasePosition();
-        
-        this.syncVelocity(deltaTime, time);              
-
-        this.applyOffset();        
+    update(deltaTime, time, { alcohol = false, earthquake = false } = {}) {
+        this.follow(deltaTime, time);
     }
 
-    setBasePosition() {
-        this.pos.x = Math.max(0, this.entity.pos.x - 100);
-    }
-
-    applyOffset() {
-        this.pos.x += this.cameraOffset.x;
-        this.pos.y += this.cameraOffset.y;
-    }
-
-    syncVelocity(deltaTime, time) {
-        if(this.cameraOffset.x > 0) {
-            this.cameraOffset.x = (300 - this.cameraOffset.x) * easeIn(time / 10000, 1)
-        } else {
-            this.cameraOffset.x = 300 * easeIn(time / 10000, 1)            
+    follow(deltaTime, time) {
+        if (!this.entity || time < 1000) {
+            return;
         }
 
-        this.lastVel = this.entity.vel.clone();
+        let entityX = this.entity.pos.x - this.cameraOffset.x;
+
+        if (Math.abs(this.cam.pos.x - entityX) > 0.1) {
+            entityX = lerp(
+                this.cam.pos.x,
+                entityX,
+                1 / this.damping * deltaTime
+            );
+        }
+
+        this.cam.pos.x = entityX;
     }
-}
-
-function linearTween(t, b, c, d) {
-	return c*t/d + b;
-};
-
-function easeInOutQuad(t, b, c, d) {
-	t /= d/2;
-	if (t < 1) return c/2*t*t + b;
-	t--;
-	return -c/2 * (t*(t-2) - 1) + b;
-};
-
-
-function easeIn(t, d){
-  return Math.pow(t / d, 5);
 }
--- js/math.js
+++ js/math.js
@@ -42,3 +42,7 @@ export class Vec2 {
 export function getRandomInt(min, max) {
     return Math.floor(Math.random() * (++max - min)) + min;
 }
+
+export function lerp(v0, v1, t) {
+    return v0 * (1 - t) + v1 * t;
+}
